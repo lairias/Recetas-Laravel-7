32a4b0f1934afedd85fb238eb7f63bf7355ef5db
lairias <lairias@unah.hn>
2021-05-26 12:14:31 -0600

    Validaciones de rutas HTTP de usuario registrado

--- database/migrations/2021_05_24_203052_create_recetas_table.php
+++ database/migrations/2021_05_24_203052_create_recetas_table.php
@@ -13,9 +13,22 @@ class CreateRecetasTable extends Migration
      */
     public function up()
     {
+
+        
+        Schema::create('categoria_recetas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->timestamps();
+        });
+
         Schema::create('recetas', function (Blueprint $table) {
             $table->id();
             $table->string('titulo');
+            $table->text('ingredientes');
+            $table->text('preparacion');
+            $table->string('imagen');
+            $table->foreignId('user_id')->references('id')->on('users')->comment('El usuario que crea la receta');
+            $table->foreignId('categoria_id')->index('id')->on('categoria_recetas')->comment('Categoria que pertenece la receta');
             $table->timestamps();
         });
     }
@@ -28,5 +41,7 @@ class CreateRecetasTable extends Migration
     public function down()
     {
         Schema::dropIfExists('recetas');
+        Schema::dropIfExists('categoria_recetas');
+
     }
 }
