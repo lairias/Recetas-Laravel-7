0f2391615160fdcd1aa8aa5f96704a1e086a5fc6
lairias <lairias@unah.hn>
2021-05-27 21:53:02 -0600

    Agregando seeder de los usuarios

---
+++ database/seeds/UsuarioSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsuarioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Luis',
+            'email' => 'correo@correo.com',
+            'password' => Hash::make("12345678"),
+            'url' => 'http://www.codigoconluis.com',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+    }
+}
